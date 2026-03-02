c5aeaed637bf36dab349dfe1dc03879d6aa0ae3c
Pedro Lucas <plfm3k@gmail.com>
2026-03-02 15:42:55 -0400

    refactor: altera método getClubs para retornar vetor de ClubModel

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -2,17 +2,16 @@ import { ClubModel } from "../models/club-model"
 import fs from "fs/promises"
 
 const getClubs = async () => {
-    return await fs.readFile("./src/data/clubs.json", "utf-8")
-}
-
-export const findAllClubs = async (): Promise<ClubModel[]> => {
-    const data = await getClubs()
+    const data = await fs.readFile("./src/data/clubs.json", "utf-8")
     const clubs: ClubModel[] = JSON.parse(data)
     return clubs
 }
 
+export const findAllClubs = async (): Promise<ClubModel[]> => {
+    return await getClubs()
+}
+
 export const findClubById = async (id: number): Promise<ClubModel | undefined> => {
-    const data = await getClubs()
-    const clubs: ClubModel[] = JSON.parse(data)
+    const clubs = await getClubs()
     return clubs.find(club => club.id === id)
 }
