eb9c6e6f8ef3d2ca556edeaf9e59839460786e37
Pedro Lucas <plfm3k@gmail.com>
2026-03-07 21:25:06 -0400

    test: implementa testes do clubs-controller

--- src/tests/controllers/clubs-controller.spec.ts
+++ src/tests/controllers/clubs-controller.spec.ts
@@ -1,11 +1,11 @@
-import { getClubs, postClub } from "../../controllers/clubs-controller"
-import { createClubService, getClubService } from "../../services/clubs-service"
+import { deleteClub, getClubById, getClubs, postClub, updateClub } from "../../controllers/clubs-controller"
+import { createClubService, deleteClubService, getClubByIdService, getClubService, updateClubService } from "../../services/clubs-service"
 import { mockRequest } from "../mocks/mockRequest"
 import { mockResponse } from "../mocks/mockResponse"
 
 jest.mock("../../services/clubs-service.ts")
 
-describe("Clubs Controller", () => {
+describe("getClubs", () => {
     test("Should return clubs successfully with code 200", async () => {
         const req = mockRequest()
         const res = mockResponse()
@@ -27,6 +27,55 @@ describe("Clubs Controller", () => {
         ])
     })
 
+    test("Should return no content with code 204", async () => {
+        const req = mockRequest()
+        const res = mockResponse()
+
+        ;(getClubService as jest.Mock).mockResolvedValue({
+            statusCode: 204,
+            body: null
+        })
+
+        await getClubs(req, res)
+        expect(getClubService).toHaveBeenCalledTimes(1)
+        expect(res.status).toHaveBeenCalledWith(204)
+        expect(res.json).toHaveBeenCalledWith(null)
+    })
+})
+
+describe("getClubById", () => {
+    test("Should return club with code 200", async () => {
+        const req = mockRequest({params: {id: "1"}})
+        const res = mockResponse()
+
+        ;(getClubByIdService as jest.Mock).mockResolvedValue({
+            statusCode: 200,
+            body: {name: "Barcelona FC"}
+        })
+
+        await getClubById(req, res)
+        expect(getClubByIdService).toHaveBeenCalledTimes(1)
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith({name: "Barcelona FC"})
+    })
+
+    test("Should return null with code 204", async () => {
+        const req = mockRequest({params: {id: "1"}})
+        const res = mockResponse()
+
+        ;(getClubByIdService as jest.Mock).mockResolvedValue({
+            statusCode: 204,
+            body: null
+        })
+
+        await getClubById(req, res)
+        expect(getClubByIdService).toHaveBeenCalledTimes(1)
+        expect(res.status).toHaveBeenCalledWith(204)
+        expect(res.json).toHaveBeenCalledWith(null)
+    })
+})
+
+describe("postClub", () => {
     test("Should creat club successfully with code 201", async () => {
         const req = mockRequest({
             body: {
@@ -47,5 +96,101 @@ describe("Clubs Controller", () => {
         expect(createClubService).toHaveBeenCalledWith({name: "Barcelona FC"})
         expect(res.status).toHaveBeenCalledWith(201)
         expect(res.json).toHaveBeenCalledWith({name: "Barcelona FC"})
-    })    
+    })
+
+    test("Should return bad request with code 400", async () => {
+        const req = mockRequest()
+        const res = mockResponse()
+
+        ;(createClubService as jest.Mock).mockResolvedValue({
+            statusCode: 400,
+            body: null
+        })
+
+        await postClub(req, res)
+        expect(createClubService).toHaveBeenCalledTimes(1)
+        expect(createClubService).toHaveBeenCalledWith({})
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith(null)
+    })
+})
+
+describe("updateClub", () => {
+    test("Should update club with code 200", async () => {
+        const id = "1"
+        const club = {name: "Barcelona FC"}
+
+        const req = mockRequest({body: club, params: {id}})
+        const res = mockResponse()
+
+        ;(updateClubService as jest.Mock).mockResolvedValue({
+            statusCode: 200,
+            body: club
+        })
+
+        await updateClub(req, res)
+        expect(updateClubService).toHaveBeenCalledTimes(1)
+        expect(updateClubService).toHaveBeenCalledWith(parseInt(id), club)
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith(club)
+    })
+
+    test("Should return bad request with code 400", async () => {
+        const id = "1"
+        const club = {name: "Barcelona FC"}
+
+        const req = mockRequest({body: club, params: {id}})
+        const res = mockResponse()
+
+        ;(updateClubService as jest.Mock).mockResolvedValue({
+            statusCode: 400,
+            body: null
+        })
+
+        await updateClub(req, res)
+        expect(updateClubService).toHaveBeenCalledTimes(1)
+        expect(updateClubService).toHaveBeenCalledWith(parseInt(id), club)
+        expect(res.status).toHaveBeenCalledWith(400)
+        expect(res.json).toHaveBeenCalledWith(null)
+    })
+})
+
+describe("deleteClub", () => {
+    test("Should return ok with message 'deleted'", async () => {
+        const id = "1"
+
+        const req = mockRequest({params: {id}})
+        const res = mockResponse()
+
+        ;(deleteClubService as jest.Mock).mockResolvedValue({
+            statusCode: 200,
+            body: {
+                message: "deleted"
+            }
+        })
+
+        await deleteClub(req, res)
+        expect(deleteClubService).toHaveBeenCalledTimes(1)
+        expect(deleteClubService).toHaveBeenCalledWith(parseInt(id))
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith({message: "deleted"})
+    })
+
+    test("Should return no content with body null", async () => {
+        const id = "1"
+
+        const req = mockRequest({params: {id}})
+        const res = mockResponse()
+
+        ;(deleteClubService as jest.Mock).mockResolvedValue({
+            statusCode: 204,
+            body: null
+        })
+
+        await deleteClub(req, res)
+        expect(deleteClubService).toHaveBeenCalledTimes(1)
+        expect(deleteClubService).toHaveBeenCalledWith(parseInt(id))
+        expect(res.status).toHaveBeenCalledWith(204)
+        expect(res.json).toHaveBeenCalledWith(null)
+    })
 })
--- src/tests/mocks/mockRequest.ts
+++ src/tests/mocks/mockRequest.ts
@@ -1,15 +1,19 @@
-import { Request } from "express";
+import { Request } from "express"
 
-type MockRequestData = {
-    body?: any,
-    params?: any,
-    query?: any
+type MockRequestData<P = any, B = any, Q = any> = {
+    body?: B
+    params?: P
+    query?: Q
 }
 
-export const mockRequest = ({body = {}, params = {}, query = {}}: MockRequestData = {}) => {
+export const mockRequest = <P = any, B = any, Q = any>({
+    body = {} as B,
+    params = {} as P,
+    query = {} as Q
+}: MockRequestData<P, B, Q> = {}) => {
     return {
         body,
         params,
         query
-    } as Request
+    } as Request<P, any, B, Q>
 }
